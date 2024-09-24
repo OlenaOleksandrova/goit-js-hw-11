c7322108e4bbec1687bceffdb5da836ee93c71a0
Olena Oleksandrova <ha240886kev@gmail.com>
2024-09-24 22:31:35 +0300

    js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,42 +1,21 @@
+
 export const BASE_URL = 'https://pixabay.com/api/';
 export const API_KEY = '46110623-cffeede45492ee1adb33fe3c4';
 
-// // Функція для запиту зображень
-// export const fetchImages = params => {
-//     const urlParams = new URLSearchParams(params);
-//     // console.log('fetchImages urlParams:', urlParams.toString());
-
-//     return fetch(`${BASE_URL}?key=${API_KEY}&${urlParams}`
-//     )
-//         .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Sorry, there are no images matching your search query. Please try again!');
-//         }
-//         return response.json();
-//     })
-//     .then(data => data.hits);
-// };
-
 export function fetchImages(query) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-
-  return fetch(`${BASE_URL}?${params}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch images');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        throw new Error('No images found');
-      }
-      return data.hits;
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
     });
+
+    return fetch(`${BASE_URL}?${params}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Sorry, there are no images matching your search query. Please try again!');
+            }
+            return response.json();
+        });
 }
--- src/main.js
+++ src/main.js
@@ -1,116 +1,52 @@
 
-// import { BASE_URL, API_KEY } from "./js/pixabay-api.js";
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-// function fetchImages(params) {
-//     const urlParams = new URLSearchParams(params);
-//     console.log('fetchImages urlParams:', urlParams.toString());
-
-//     return fetch(`${BASE_URL}?key=${API_KEY}&${urlParams}`).then(response => {
-//         if (!response.ok) {
-//             throw new Error('Sorry, there are no images matching your search query. Please try again!');
-//         }
-//         return response.json();
-//     });
-// }
-
-// const params = {
-//     key: '46110623-cffeede45492ee1adb33fe3c4',
-//     q: '',
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: 'true'
-// };
-
-
-// const imagesBoxEl = document.querySelector('.gallery');
-
-// fetchImages(params).then(data => {
-//     const { hits } = data;
-    
-//     const imagesMarkup = hits.reduce((acc, item) => {
-//         return (
-//             acc +
-//             `<article class="gallery-item">
-//                 <a href="${item.largeImageURL}" class="gallery-link">
-//                  <img src="${item.webformatURL}" alt="${item.tags}" class="gallery-image" />
-//                  </a>
-//                  <ul class="info">
-//                 < liclass="info-item">Likes: ${item.likes}<li/>
-//                 <li class="info-item">Views: ${item.views}</li>
-//                 <li class="info-item">Comments: ${item.comments}</li>
-//                 <li class="info-item">Downloads: ${item.downloads}</li>
-//                 </ul>
-//             </article>`
-//         );
-//     }, '');
-
-//     imagesBoxEl.innerHTML = imagesMarkup;
-
-//     const images = new SimpleLightbox('.gallery a', {
-//             captions: true,
-//             captionDelay: 250,
-//     });
-//     console.log(images);
-            
-// }).catch(error => {
-//     iziToast.error({
-//         title: 'Sorry, there are no images matching your search query. Please try again!',
-//         message: error.message,
-   
-//     });
-//  });
-
-
-
-
-
 import { fetchImages } from './js/pixabay-api.js';
-import { renderImages, showLoader, hideLoader } from './js/render-functions';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import { renderImages } from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// import { BASE_URL, API_KEY } from "./js/pixabay-api.js";
-
-// Виправлення на формі
 const searchForm = document.querySelector('.search-form');
- const gallery = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionDelay: 250,
- });
-    
+const imagesBoxEl = document.querySelector('.gallery');
+let gallery = null;
+
+// подія форми 
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    const query = event.target.elements.query.value.trim(); // Отримуємо значення введеного пошукового запиту
-    if (!query) {
-        return;
-    };
     
-    showLoader();
+    const query = event.target.elements.query.value.trim();
 
-  fetchImages(query)
-    .then(images => {
-      renderImages(images);
-      gallery.refresh();
-    })
-      .catch(error => {
-        
-    iziToast.error({
-        title: 'Error',
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-   
-    });
-    })
-    .finally(() => {
-      hideLoader();
-    });
+    if (!query) {
+        return; 
+    }
+
+    imagesBoxEl.innerHTML = '';
+
+    fetchImages(query)
+        .then(data => {
+            const { hits } = data;
+            
+            if (hits.length === 0) {
+                throw new Error('No images found for this search query.');
+            }
+
+            const imagesMarkup = renderImages(hits);
+            imagesBoxEl.innerHTML = imagesMarkup;
+
+            //SimpleLightbox
+            if (gallery) {
+                gallery.refresh();
+            } else {
+                gallery = new SimpleLightbox('.gallery a', {
+                    captions: true,
+                    captionDelay: 250,
+                });
+            }
+        })
+        .catch(error => {
+            iziToast.error({
+                title: 'Error',
+                message: error.message,
+            });
+        });
 });
-
-
